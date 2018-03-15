fd8af2cb85019eed1e7dadccf8d73bd4e87639dc
wq521193913 <521193913@qq.com>
2018-03-15 17:56:25 +0800

    201803151756

--- broker/controller/src/main/java/com/broker/startup/BrokerApplication.java
+++ broker/controller/src/main/java/com/broker/startup/BrokerApplication.java
@@ -1,6 +1,7 @@
 package com.broker.startup;
 
 import org.apache.log4j.Logger;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 //@ImportResource(locations = {"classpath:config/application-*.xml"})
 @ComponentScan(value = {"com.broker"})
+@MapperScan("com.broker.dao")
 public class BrokerApplication {
 
 	final Logger logger = Logger.getLogger(BrokerApplication.class);
