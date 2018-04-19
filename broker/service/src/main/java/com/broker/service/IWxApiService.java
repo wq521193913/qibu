78c8775c48b29a39980c563fe5bf841828be6566
wq521193913 <521193913@qq.com>
2018-04-19 23:34:17 +0800

    1

---
+++ broker/service/src/main/java/com/broker/service/IWxApiService.java
@@ -0,0 +1,12 @@
+package com.broker.service;
+
+/**
+ * @author: Administrator
+ * @description: 微信API
+ * @date: Create in 2018/4/19 0019 下午 9:22
+ * @modified:
+ */
+public interface IWxApiService {
+
+
+}
